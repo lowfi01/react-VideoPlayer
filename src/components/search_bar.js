e9a2159e7f6a0c48667752705438d14b554fdc1b
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-08-28 20:02:33 +1000

    Created Event Handler for SearchBar input - implement arrow function to onChange & left comments to see evolution of code & examples

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,9 +9,20 @@ class SearchBar extends Component {
     // render function
     render(){
         return (
-            <input />
-        ); // return JSX
+                // note - onChange is the event emitter used by html
+                //<input onChange={this.onInputChange}/>
+                //<input onChange={(event) => {console.log(event.target.value);}}/> // arrow function
+                <input onChange={event => console.log(event.target.value)}/> // cleaner arrow function
+            ); // return JSX
     }
+
+    // // Event Handler for input 
+    // onInputChange(event){  // event object is always passed by onChange
+    //     // When input changes - run this code
+    //     console.log(event.target.value); //output = text written within input box
+    // }
+
+    
 }
 
 
